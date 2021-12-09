430474ee12ec0e45e6c0394dc43eb0b01ed0bbad
Kelton Santos <keltongarciasantos@gmail.com>
2021-12-08 23:52:14 -0100

    08/12/2021

--- R_codes_examples/Rcodes.R
+++ R_codes_examples/Rcodes.R
@@ -1,16 +1,13 @@
-# purely random process with mean 0 and standard deviation 1.5
-library("ggplot2")
-eps <- rnorm(100, mean = 0, sd = 1)
-mu <- 2 
-X_t <- mu + eps
-#ts.plot(X_t, main = "Exemplo of série temporal estacionária", xlab = "Tempo", ylab = expression(X[t]), col = "blue")
-ggplot(X_t, aes('Exemplo de série temporal estacionária','Tempo', colour = class)) + 
-  geom_point( )
-
-attach(Pib_anual)
 library(ggplot2)
-library(psych)
+library(scales)
+library(lubridate)
+library(readxl)
+entrada<- read_excel("R_codes_examples/entrada_turistas.xlsx", sheet = "mensal", col_types = c("date", "numeric"))
+ggplot(entrada, aes(x = entrada$`mes_ano`, y = entrada$`movimento_de_passageiros`)) +
+  geom_line( colour = "#0c4c8a") + 
+  scale_x_date(date_breaks = "6 months",labels = date_format("%b-%Y"),limits = c(as.Date("2006-08-01"), NA)) +
+  scale_y_continuous(labels=function(n){format(n, scientific = FALSE)}) +
+  labs(y= "Movimento de Passageiros mensais 2006 a 2017 ", x = "Mês/Ano") +
+  xlab("") +
+  theme(axis.text.x=element_text(angle=60, hjust=1))
 
-describe(Pib_anual)
-ggplot(Pib_anual, aes('Exemplo de série temporal estacionária','Tempo')) + 
-  geom_line( )
